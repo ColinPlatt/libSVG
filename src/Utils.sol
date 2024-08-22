ef4b5359c211e237679ada8b2be66bb7f8185d24
Colin Platt <colingplatt@gmail.com>
2024-08-22 16:17:35 +0200

    adding solady

--- src/Utils.sol
+++ src/Utils.sol
@@ -1,36 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
+import "Solady/utils/LibString.sol";
+
 // Core utils used extensively to format CSS and numbers.
 library utils {
     // used to simulate empty strings
-    string internal constant NULL = '';
+    string internal constant NULL = "";
 
     // formats a CSS variable line. includes a semicolon for formatting.
-    function setCssVar(string memory _key, string memory _val)
-        internal
-        pure
-        returns (string memory)
-    {
-        return string.concat('--', _key, ':', _val, ';');
+    function setCssVar(string memory _key, string memory _val) internal pure returns (string memory) {
+        return string.concat("--", _key, ":", _val, ";");
     }
 
     // formats getting a css variable
-    function getCssVar(string memory _key)
-        internal
-        pure
-        returns (string memory)
-    {
-        return string.concat('var(--', _key, ')');
+    function getCssVar(string memory _key) internal pure returns (string memory) {
+        return string.concat("var(--", _key, ")");
     }
 
     // formats getting a def URL
-    function getDefURL(string memory _id)
-        internal
-        pure
-        returns (string memory)
-    {
-        return string.concat('url(#', _id, ')');
+    function getDefURL(string memory _id) internal pure returns (string memory) {
+        return string.concat("url(#", _id, ")");
     }
 
     // formats rgba white with a specified opacity / alpha
@@ -44,187 +34,115 @@ library utils {
     }
 
     // formats generic rgba color in css
-    function rgba(
-        uint256 _r,
-        uint256 _g,
-        uint256 _b,
-        uint256 _a
-    ) internal pure returns (string memory) {
-        string memory formattedA = _a < 100
-            ? string.concat('0.', utils.toString(_a))
-            : '1';
-        return
-            string.concat(
-                'rgba(',
-                utils.toString(_r),
-                ',',
-                utils.toString(_g),
-                ',',
-                utils.toString(_b),
-                ',',
-                formattedA,
-                ')'
-            );
-    }
-
-    function cssBraces(
-        string memory _attribute, 
-        string memory _value
-    )   internal
-        pure
-        returns (string memory)
-    {
+    function rgba(uint256 _r, uint256 _g, uint256 _b, uint256 _a) internal pure returns (string memory) {
+        string memory formattedA = _a < 100 ? string.concat("0.", LibString.toString(_a)) : "1";
         return string.concat(
-            ' {',
-            _attribute,
-            ': ',
-            _value,
-            '}'
+            "rgba(",
+            LibString.toString(_r),
+            ",",
+            LibString.toString(_g),
+            ",",
+            LibString.toString(_b),
+            ",",
+            formattedA,
+            ")"
         );
     }
 
-    function cssBraces(
-        string[] memory _attributes, 
-        string[] memory _values
-    )   internal
-        pure
-        returns (string memory)
-    {
+    function cssBraces(string memory _attribute, string memory _value) internal pure returns (string memory) {
+        return string.concat(" {", _attribute, ": ", _value, "}");
+    }
+
+    function cssBraces(string[] memory _attributes, string[] memory _values) internal pure returns (string memory) {
         require(_attributes.length == _values.length, "Utils: Unbalanced Arrays");
-        
+
         uint256 len = _attributes.length;
 
-        string memory results = ' {';
+        string memory results = " {";
 
-        for (uint256 i = 0; i<len; i++) {
-            results = string.concat(
-                                    results, 
-                                    _attributes[i],
-                                    ': ',
-                                    _values[i],
-                                     '; '
-                                    );
-                                    
+        for (uint256 i = 0; i < len; i++) {
+            results = string.concat(results, _attributes[i], ": ", _values[i], "; ");
         }
 
-        return string.concat(results, '}');
+        return string.concat(results, "}");
     }
 
     //deals with integers (i.e. no decimals)
     function points(uint256[2][] memory pointsArray) internal pure returns (string memory) {
         require(pointsArray.length >= 3, "Utils: Array too short");
 
-        uint256 len = pointsArray.length-1;
-
+        uint256 len = pointsArray.length - 1;
 
         string memory results = 'points="';
 
-        for (uint256 i=0; i<len; i++){
+        for (uint256 i = 0; i < len; i++) {
             results = string.concat(
-                                    results, 
-                                    toString(pointsArray[i][0]), 
-                                    ',', 
-                                    toString(pointsArray[i][1]),
-                                    ' '
-                                    );
+                results, LibString.toString(pointsArray[i][0]), ",", LibString.toString(pointsArray[i][1]), " "
+            );
         }
 
         return string.concat(
-                            results, 
-                            toString(pointsArray[len][0]), 
-                            ',', 
-                            toString(pointsArray[len][1]),
-                            '"'
-                            );
+            results, LibString.toString(pointsArray[len][0]), ",", LibString.toString(pointsArray[len][1]), '"'
+        );
     }
 
-    // allows for a uniform precision to be applied to all points 
+    // allows for a uniform precision to be applied to all points
     function points(uint256[2][] memory pointsArray, uint256 decimalPrecision) internal pure returns (string memory) {
         require(pointsArray.length >= 3, "Utils: Array too short");
 
-        uint256 len = pointsArray.length-1;
-
+        uint256 len = pointsArray.length - 1;
 
         string memory results = 'points="';
 
-        for (uint256 i=0; i<len; i++){
+        for (uint256 i = 0; i < len; i++) {
             results = string.concat(
-                                    results, 
-                                    toString(pointsArray[i][0], decimalPrecision), 
-                                    ',', 
-                                    toString(pointsArray[i][1], decimalPrecision),
-                                    ' '
-                                    );
+                results,
+                toString(pointsArray[i][0], decimalPrecision),
+                ",",
+                toString(pointsArray[i][1], decimalPrecision),
+                " "
+            );
         }
 
         return string.concat(
-                            results, 
-                            toString(pointsArray[len][0], decimalPrecision), 
-                            ',', 
-                            toString(pointsArray[len][1], decimalPrecision),
-                            '"'
-                            );
+            results,
+            toString(pointsArray[len][0], decimalPrecision),
+            ",",
+            toString(pointsArray[len][1], decimalPrecision),
+            '"'
+        );
     }
 
     // checks if two strings are equal
-    function stringsEqual(string memory _a, string memory _b)
-        internal
-        pure
-        returns (bool)
-    {
-        return
-            keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b));
+    function stringsEqual(string memory _a, string memory _b) internal pure returns (bool) {
+        return keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b));
     }
 
     // returns the length of a string in characters
-    function utfStringLength(string memory _str)
-        internal
-        pure
-        returns (uint256 length)
-    {
+    function utfStringLength(string memory _str) internal pure returns (uint256 length) {
         uint256 i = 0;
         bytes memory string_rep = bytes(_str);
 
         while (i < string_rep.length) {
-            if (string_rep[i] >> 7 == 0) i += 1;
-            else if (string_rep[i] >> 5 == bytes1(uint8(0x6))) i += 2;
-            else if (string_rep[i] >> 4 == bytes1(uint8(0xE))) i += 3;
-            else if (string_rep[i] >> 3 == bytes1(uint8(0x1E)))
+            if (string_rep[i] >> 7 == 0) {
+                i += 1;
+            } else if (string_rep[i] >> 5 == bytes1(uint8(0x6))) {
+                i += 2;
+            } else if (string_rep[i] >> 4 == bytes1(uint8(0xE))) {
+                i += 3;
+            } else if (string_rep[i] >> 3 == bytes1(uint8(0x1E))) {
                 i += 4;
-                //For safety
-            else i += 1;
+            }
+            //For safety
+            else {
+                i += 1;
+            }
 
             length++;
         }
     }
 
-     /**
-     * taken from Openzeppelin
-     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
-     */
-    function toString(uint256 value) internal pure returns (string memory) {
-        // Inspired by OraclizeAPI's implementation - MIT licence
-        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
-
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
-
-        // allows the insertion of a decimal point in the returned string at precision
+    // allows the insertion of a decimal point in the returned string at precision
     function toString(uint256 value, uint256 precision) internal pure returns (string memory) {
         // Inspired by OraclizeAPI's implementation - MIT licence
         // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
@@ -239,16 +157,16 @@ library utils {
             temp /= 10;
         }
         require(precision <= digits && precision > 0, "Utils: precision invalid");
-        precision == digits ? digits +=2 : digits++; //adds a space for the decimal point, 2 if it is the whole uint
-        
+        precision == digits ? digits += 2 : digits++; //adds a space for the decimal point, 2 if it is the whole uint
+
         uint256 decimalPlacement = digits - precision - 1;
         bytes memory buffer = new bytes(digits);
-        
+
         buffer[decimalPlacement] = 0x2E; // add the decimal point, ASCII 46/hex 2E
         if (decimalPlacement == 1) {
             buffer[0] = 0x30;
         }
-        
+
         while (value != 0) {
             digits -= 1;
             if (digits != decimalPlacement) {
@@ -259,5 +177,4 @@ library utils {
 
         return string(buffer);
     }
-
 }
